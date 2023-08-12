a2ece642ceb32eb18156436c58a3262dc41034d8
Jaeman Kim <daga4242@gmail.com>
2023-08-12 13:08:56 +0900

    confirmation submit

--- src/features/auth/routes/Login.jsx
+++ src/features/auth/routes/Login.jsx
@@ -6,11 +6,14 @@ import { validate } from 'utils/validate';
 import { useLogin, useUser } from 'library/auth';
 import { loginWithPhoneNumber } from '../api/login';
 import { appVerifier, firebaseAuth } from 'library/firebase';
+import { signInWithCredential } from 'firebase/auth';
 
 export const Login = () => {
   // const login = useUser();
   const [phoneNumber, setPhoneNumber] = useState();
   const [isValidated, setIsValidated] = useState(false);
+  const [confirmationResult, setConfirmationResult] = useState();
+  const [confirmationCode, setConfirmationCode] = useState('');
   useEffect(() => {
     setIsValidated(validatePhoneNumber(phoneNumber));
   }, [phoneNumber]);
@@ -20,15 +23,21 @@ export const Login = () => {
   };
 
   const handleSubmit = async (values) => {
-    console.log(appVerifier);
     if (isValidated) {
-      console.log('actived!');
       loginWithPhoneNumber(firebaseAuth, phoneNumber, appVerifier)
-        .then((res) => console.log(res))
+        .then((res) => {
+          setConfirmationResult(res);
+        })
         .catch((error) => console.error(error));
-    } else {
-      console.log('wrong');
     }
+  };
+
+  const handleConfirmationSubmit = () => {
+    console.log(confirmationResult);
+    confirmationResult
+      .confirm(confirmationCode)
+      .then((res) => console.log(res))
+      .catch((error) => console.error(error));
   };
   return (
     <Wrapper>
@@ -53,12 +62,42 @@ export const Login = () => {
                 variant={isValidated ? 'transparent' : 'transparentGray'}
                 borderColor="lightGray"
               >
-                인증문자 받기
+                {confirmationResult ? '인증문자 다시받기' : '인증문자 받기'}
               </Button>
             </Box>
           );
         }}
       </Form>
+      {confirmationResult && (
+        <Form onSubmit={handleConfirmationSubmit}>
+          {({ register, formState }) => {
+            return (
+              <Box gap="15px">
+                <InputField
+                  maxLength="11"
+                  type="number"
+                  borderRadius="5px"
+                  getValue={setConfirmationCode}
+                  registraion={register('confirmationCode')}
+                  error={formState.errors['confirmationCode']}
+                  placeholder="인증번호 입력"
+                  required
+                />
+                <Button
+                  width="100%"
+                  borderRadius="5px"
+                  fontWeight="600"
+                  size="large"
+                  variant={isValidated ? 'transparent' : 'transparentGray'}
+                  borderColor="lightGray"
+                >
+                  동의하고 시작하기
+                </Button>
+              </Box>
+            );
+          }}
+        </Form>
+      )}
     </Wrapper>
   );
 };
